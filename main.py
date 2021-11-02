7f9353b805c9b3d2f6039aee8356220797968a6d
Cancri <81624686+pravda-cancri@users.noreply.github.com>
2021-11-02 10:50:41 +0000

    merged

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os;clear=lambda:os.system("clear");un=[".upm","extra",".git"]
 def go_somewhere(_,_1):clear();os.system("python "+_1+"/"+os.listdir(_1)[_-2]);input("\npress enter to go to main menu\n")
 def output(list1):
     for f in range (len(os.listdir(list1))):print(f+1,"--",os.listdir(list1)[f-1])
-    x=int(input("input the number of the project you want to run\nnote all programms wth 'extra' in their names is just that... they are not to be used for marking\n"));return x;clear()
+    x=int(input("input the number of the project you want to run\nnote all programms wth 'extra' in their names is just that... they are not to be used for marking\n"));return x
 def main():
     __=([(i)for i in list(filter(os.path.isdir,os.listdir("/home/runner/obs-all")))if i not in un])
     for x in range(len(__)):print(x+1,"--",__[x])
