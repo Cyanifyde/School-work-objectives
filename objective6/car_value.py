b836378ce48162a595d81849d848b207287efd51
Cancri <81624686+pravda-cancri@users.noreply.github.com>
2021-11-05 10:33:25 +0000

    car value problem

---
+++ objective6/car_value.py
@@ -0,0 +1,25 @@
+"""
+Write a program that illustrates the depreciation of the value of a car. The program takes three parameters, the year, the value of the car and the minimum resale value. The car depreciates by 30% in the first and second year, and 20% in each subsequent year. The program should output the values from the model until the resale value is reached. E.g.
+Car is bought for £12500. Minimum resale value is £6000.
+2020: £12500
+2021: £8750
+2022: £6125
+Part exchange in 2022.
+"""
+
+def depreciates(year,value,minimum):
+    years=0
+    reached=False
+    while reached==False:
+        print("{} : £{}".format(year,value))
+        if value>minimum and years ==0 or 1:
+            value-=(0.3*value)
+        else:
+            value-=(0.2*value)
+        if value<minimum:
+            return year
+        year+=1
+        years+=1
+x=depreciates(2020,12500,6000)
+print("part exchange in {}".format(x))
+
