0af9ad4a24c2b9bb39ad63b5d7b9d677f2b25aba
Cancri <81624686+pravda-cancri@users.noreply.github.com>
2021-11-23 10:56:41 +0000

    all

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ DO NOT CHANGE main.py or any file in /change
 """
 x=True #change this to  False to run change log updater
 
-import os,change.changelog;clear=lambda:os.system("clear");un=[".upm","extra",".git","change"];exec("if x==False:change.changelog.a()")
-def send(__,_,x):clear();os.system("python "+__[_]+"/"+os.listdir(__[_])[x-2]);_=("" if input("\npress enter to go to rerun\nor input anything else to return\n")!="" else "1\n"+str(_)+"\n"+str(x)) ; f=open("update.txt","w");f.write(_);f.close();os.system("python main.py")
-def paths():return sorted([(i)for i in list(filter(os.path.isdir,os.listdir("/home/runner/obs-all")))if i not in un])
-def main():clear();__=paths();[print(x+1,"--",__[x]) for x in range (len(__))];_=int(input("what objective do you want to run?, enter then number next to the objective\nnote that the programs in the 'extra' directory is just extra, it is not to be used for marking as it doesnt follow the spec\n"))-1;clear();[print(f+1,"--",os.listdir(__[_])[f-1]) for f in range (len(os.listdir(__[_])))];send(__,_,int(input("input the number of the project you want to run\n")))
-exec('try:f=open("update.txt","r");f=f.read().splitlines();main() if f[0]=="0" else send(paths(),int(f[1]),int(f[2]))\nexcept:f=open("update.txt","w");f.write("0");f.close()\nos.system("python main.py")')
+import os;clear=lambda:os.system("clear")
+un=[".upm","extra",".git","change","extras","main.py","update.txt",".breakpoints","change_logs.txt","README.md","poetry.lock","pyproject.toml"];exec("if x==False:change.changelog.a()")
+def paths(x):return sorted([(i)for i in list (os.listdir(x))if i not in un])
+def send(x):clear();os.system("python "+x);_=("" if input("\npress enter to go to rerun\nor input anything else to return\n")!="" else x) ;f=open("update.txt","w");f.write(_);f.close()
+def recursion(x):v=paths(x);[print(str(x)+" --- "+v[x]) for x in range (len(v))];_=int(input("what file do you want to open / run?\n"));pathn=x+"/"+str(v[_]);clear();return pathn
+def main():
+    po=True;path="/home/runner/all"
+    while po==True:
+        try:path=recursion(path)
+        except NotADirectoryError:send(path);po=False;clear()
+f=open("update.txt","r");f=f.read()
+try:clear();send(f) if f!="" else main()
+except:f=open("update.txt","w");f.write("");f.close()
+os.system("python main.py")
