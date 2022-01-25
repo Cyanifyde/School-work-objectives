cfbb739ebd2dace1779f1d4c0bc4ca9b98c8301d
Cancri <81624686+pravda-cancri@users.noreply.github.com>
2022-01-25 09:41:29 +0000

    runhelp

--- crawl.py
+++ crawl.py
@@ -3,7 +3,7 @@ path = '/home/runner/all/School-work-objectives';list_dir=[]
 for root, directories, files in os.walk(path, topdown=False):
     for name in files:
         if files !=(".git"):
-            if name not in (".upm.gitextrasmain.py.breakpointsREADME.mdpoetry.lockpyproject.toml._runhelp.py__pycache__errors.pyfilefiles"):list_dir.append(os.path.join(root, name))
+            if name not in (".upm.gitextrasmain.py.breakpointsREADME.mdpoetry.lockpyproject.toml.__pycache__errors.pyfilefiles"):list_dir.append(os.path.join(root, name))
 f = open("_crawled.txt", "w");f = open("_crawled.txt", "a")
 for x in list_dir:f.write(x+"\n")
 f.close()
