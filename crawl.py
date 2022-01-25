64f14c1f72c65fd019d351fb1f7b124b9eb4a33b
Cancri <81624686+pravda-cancri@users.noreply.github.com>
2022-01-25 12:49:20 +0000

    lol

--- crawl.py
+++ crawl.py
@@ -1,29 +1,91 @@
-import os,time, urllib.request
-path = '/home/runner/all/School-work-objectives';list_dir=[]
-for root, directories, files in os.walk(path, topdown=False):
-    for name in files:
-        if files !=(".git"):
-            if name not in (".upm.gitextrasmain.py.breakpointsREADME.mdpoetry.lockpyproject.toml.__pycache__errors.pyfilefiles"):list_dir.append(os.path.join(root, name))
-f = open("_crawled.txt", "w");f = open("_crawled.txt", "a")
-for x in list_dir:f.write(x+"\n")
-f.close()
-response = urllib.request.urlopen('https://raw.githubusercontent.com/pravda-cancri/School-work-objectives/master/_crawled.txt');data = response.read();f = open("crawl.txt", "wb");f.write(data);f.close();f = open("crawl.txt", "r");f=f.read().splitlines();g = open("_crawled.txt", "r");g=g.read().splitlines();found=[]
-for x in f:
-    if x not in g:found.append(x)
-items=[];at=[]
-for g in found:
-    p=g.split("/");item=""
-    for x in range (5):del p[0]
-    for x in p:item+="/"+x
-    items.append(item);at.append(g)
+import os, time, urllib.request
 
-def add(item,no): time.sleep(0.5);response=urllib.request.urlopen("https://raw.githubusercontent.com/pravda-cancri/School-work-objectives/master/School-work-objectives/"+items[x]);data = response.read();f = open(at[x], "wb");f.write(data);f.close();time.sleep(0.5);print("copying to file",''.join(at[x].split('/')[-1]))
-for x in range (len(items)):
-    time.sleep(0.5)
-    try:print("downloading",''.join(at[x].split('/')[-1]));add(items,x)
-    except:
+
+def crawl():
+    path = '/home/runner/all/School-work-objectives'
+    list_dir = []
+    for root, directories, files in os.walk(path, topdown=False):
+        for name in files:
+            if files != (".git"):
+                if name not in (
+                        ".upm.gitextrasmain.py.breakpointsREADME.mdpoetry.lockpyproject.toml."
+                ):
+                    list_dir.append(os.path.join(root, name))
+    f = open("_crawled.txt", "w")
+    f = open("_crawled.txt", "a")
+    for x in list_dir:
+        f.write(x + "\n")
+    f.close()
+
+
+def download_list():
+    response = urllib.request.urlopen(
+        'https://raw.githubusercontent.com/pravda-cancri/School-work-objectives/master/_crawled.txt'
+    )
+    data = response.read()
+    f = open("crawl.txt", "wb")
+    f.write(data)
+    f.close()
+
+
+def make_dir(item):
+    os.system("mkdir " + item)
+
+
+def write_to_file(data, at):
+    print("downloading", ''.join(at.split('/')[-1]))
+    f = open(at, "wb")
+    f.write(data)
+    f.close()
+    time.sleep(0.3)
+    print("finished writing to", ''.join(at.split('/')[-1]))
+
+
+def check_dir(item):
+    var = len(item.split('/')) - 1
+    gr = ""
+    for x in range(var):
+        gr += "/" + ''.join(item.split('/')[x])
+        if not os.path.isdir(gr):
+            print("making dir", ''.join(gr.split('/')[-1]))
+            make_dir(gr)
+
+
+def download(_, item):
+    response = urllib.request.urlopen(
+        "https://raw.githubusercontent.com/pravda-cancri/School-work-objectives/master/School-work-objectives/"
+        + _)
+    data = response.read()
+    check_dir(item)
+    write_to_file(data, item)
+
+
+def download_item(item):
+    num = 2
+    running = True
+    while running:
         try:
-            _='/'.join(at[x].split('/')[0:-1]);os.mkdir(_);print("making dir",''.join(_.split('/')[-1]));add(items,x)
-        except:print("directory noneexistant")
+            _ = '/'.join(item.split('/')[-num:])
+            download(_, item)
+            running = False
+        except:
+            num += 1
+
+
+def check_different():
+    f = open("crawl.txt", "r")
+    f = f.read().splitlines()
+    g = open("_crawled.txt", "r")
+    g = g.read().splitlines()
+    for x in f:
+        if x not in g:
+            print("missing", ''.join(x.split('/')[-1]))
+            time.sleep(0.5)
+            download_item(x)
+
+
+crawl()
+download_list()
+check_different()
 glf=True
 if glf==True:os.remove("_crawled.txt");os.remove("crawl.txt")
